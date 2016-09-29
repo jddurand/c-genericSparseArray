b7c64cdc5b3cae646f3c476ff8909e495629955c
Jean-Damien Durand <jeandamiendurand@free.fr>
2016-09-29 04:54:52 +0200

    Initial version

---
+++ include/genericSparseArray.h
@@ -0,0 +1,42 @@
+#ifndef GENERICSPARSEARRAY_H
+#define GENERICSPARSEARRAY_H
+
+/* Our sparse array is built on top of genericHash */
+
+#include <stdlib.h>
+#include <genericHash.h>
+
+#define GENERICSPARSEARRAY_NEW(sparseArrayName, keyIndFunctionp) \
+  GENERICHASH_NEW(sparseArrayName, keyIndFunctionp)
+
+#define GENERICSPARSEARRAY_NEW_ALL(sparseArrayName, keyIndFunctionp, valCopyFunctionp, valFreeFunctionp, wantedSize, wantedSubSize) \
+  GENERICHASH_NEW_ALL(sparseArrayName, keyIndFunctionp, NULL, NULL, NULL, valCopyFunctionp, valFreeFunctionp, wantedSize, wantedSubSize)
+
+#define GENERICSPARSEARRAY_VALCOPYFUNCTION(sparseArrayName) \
+  GENERICHASH_VALCOPYFUNCTION(sparseArrayName)
+  
+#define GENERICSPARSEARRAY_VALFREEFUNCTION(sparseArrayName) \
+  GENERICHASH_VALFREEFUNCTION(sparseArrayName)
+
+#define GENERICSPARSEARRAY_SET(sparseArrayName, userDatavp, indice, valType, valVal) do { \
+    int keyVal = indice;						\
+    GENERICHASH_SET(sparseArrayName, userDatavp, INT, keyVal, valType, valVal);	\
+  } while (0)
+
+#define GENERICSPARSEARRAY_FIND(sparseArrayName, userDatavp, indice, valType, valValp, findResult) do {	\
+    int keyVal = indice;						\
+    GENERICHASH_FIND(sparseArrayName, userDatavp, INT, keyVal, valType, valValp, findResult); \
+  } while (0)
+
+#define GENERICSPARSEARRAY_REMOVE(sparseArrayName, userDatavp, indice, valType, valValp, findResult) do { \
+    int keyVal = indice;						\
+    GENERICHASH_REMOVE(sparseArrayName, userDatavp, INT, keyVal, valType, valValp, findResult);	\
+  } while (0)
+
+#define GENERICSPARSEARRAY_FREE(sparseArrayName, userDatavp) \
+  GENERICHASH_FREE(sparseArrayName, userDatavp)
+  
+#define GENERICSPARSEARRAY_ERROR(sparseArrayName) \
+  GENERICHASH_ERROR(sparseArrayName)
+
+#endif /* GENERICSPARSEARRAY_H */
