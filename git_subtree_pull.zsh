1d4f64065d7705d55ea78527b480d90174320088
Jean-Damien Durand <Jean-Damien.Durand@fisglobal.com>
2017-03-06 17:45:05 +0100

    git_subtree_pull.zsh

--- git_subtree_pull.zsh
+++ git_subtree_pull.zsh
@@ -5,7 +5,6 @@ foreach this (cmake-utils genericHash genericLogger) {
   git fetch $this-remote master
 }
 
-git reset --hard origin/master
 git clean -ffdx
 foreach this (cmake-utils genericHash genericLogger) {
   git subtree pull --prefix 3rdparty/github/$this-remote $this-remote master --squash
